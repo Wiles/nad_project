c94e0f03f39322655bc12888e394f891f5557fa4
TomK <6772262+tkempton@users.noreply.github.com>
2011-03-25 22:04:07 -0400

    added: user display name, user name to session

--- htdocs/profile.php
+++ htdocs/profile.php
@@ -1,22 +1,30 @@
 <?php
     session_start();
-    $user = isset($_SESSION['user_id'])?$_SESSION['user_id']:"";
+    $user = isset($_SESSION['user_id'])?$_SESSION['user_name']:"";
     if( $user == "" )
     {
         header( 'Location: /') ;
     }
     else
     {
-        echo "Profile: ".$user;
+        //echo "Profile: ".$user;
     }
 ?>
-<?php include $_SERVER['DOCUMENT_ROOT'].'/../templates/header.php'; ?>
-<form method="POST" action ="<?=$_SERVER['PHP_SELF']?>">
-    <div id="error"><?php echo $error; ?></div>
-    E-mail:<br />
-    <input type="text" name="email" value ="<?php echo $email; ?>" /><br />
-    Password:<br />
-    <input type="password" name="password" /><br /><br />
-    <input type="submit" value="Login!" />
-</form>
-<?php include $_SERVER['DOCUMENT_ROOT'].'/../templates/footer.php'; ?>
+<html>
+    <head>
+        <title>Setbook - <?php echo $user; ?></title>
+	<link rel="stylesheet" type="text/css" href="/style.css" />
+    </head>
+    <body>
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/../templates/private_header.html'; ?>
+        <form method="POST" action ="<?=$_SERVER['PHP_SELF']?>">
+            <div id="error"><?php echo $error; ?></div>
+            E-mail:<br />
+            <input type="text" name="email" value ="<?php echo $email; ?>" /><br />
+            Password:<br />
+            <input type="password" name="password" /><br /><br />
+            <input type="submit" value="Login!" />
+        </form>
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/../templates/private_footer.html'; ?>
+    </body>
+</html>
