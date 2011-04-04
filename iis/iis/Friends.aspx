42c238b7fe9c797b71c16732a5cc4a5481e0811b
hedgehog89 <hedgehog89@localhost>
2011-04-04 18:18:46 -0400

    Added: Notification, blank friends page as place holder

---
+++ iis/iis/Friends.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Friends.aspx.cs" Inherits="iis.Friends" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <script type="text/javascript" src="notify.js"></script>
+</head>
+<body onload="getPostCount()">
+    <!--#include FILE="private_header.html" --> 
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>    
+    <!--#include FILE="private_footer.html" --> 
+</body>
+</html>
---
+++ iis/iis/Notification.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data.Odbc;
+
+namespace iis
+{
+    public partial class Notification : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Clear();
+
+            OdbcConnection conn = null;
+            OdbcCommand cmd;
+            OdbcDataReader reader;
+
+            string newPosts = "0";
+
+            if (Session["User_ID"] != null)
+            {
+                string query = "SELECT lastactive FROM users where id='" + Session["user_id"] + "'";
+                try
+                {
+                    conn = new OdbcConnection(Shared.ConnectionString());
+                    conn.Open();
+
+                    cmd = new OdbcCommand(query);
+                    cmd.Connection = conn;
+                    reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        query = "SELECT count(*) FROM post WHERE parent IS NULL AND time > '" + reader[0] + "' AND profileid='" + Session["user_id"] + "'";
+
+                        cmd = new OdbcCommand(query);
+                        cmd.Connection = conn;
+                        reader = cmd.ExecuteReader();
+                        if (reader.HasRows)
+                        {
+                            newPosts = reader[0].ToString();
+                        }
+                    }
+                }
+                catch
+                {
+                }
+            }
+            Response.Write(newPosts);
+            Response.End();
+        }
+    }
+}
