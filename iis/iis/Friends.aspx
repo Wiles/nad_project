8da3ad822b0b39d042d24405195d8010d1970c47
Copeland <Copeland@Copeland-PC>
2011-04-10 20:43:06 -0400

    page changed to asp.net, just need to do the add request and accept require

--- iis/iis/Friends.aspx
+++ iis/iis/Friends.aspx
@@ -4,14 +4,33 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+     <!--<title>SetBook - <?php echo $user; ?> - Friends</title>-->
+
     <link rel="stylesheet" type="text/css" href="style.css" />
-    <script type="text/javascript" src="notify.js"></script>
+    <script type="text/javascript" src="notify.js">
+     function load_profile(id)
+        {
+            document.getElementsByName(profileload).value = id;
+        }
+    </script>
 </head>
 <body onload="getPostCount()">
     <!--#include FILE="private_header.html" --> 
     <form id="form1" runat="server">
     <div>
+
+    <input type="hidden" name="profileload" value="0"/>
+    <asp:Label ID="lb_error" runat="server" ></asp:Label>
+
+    Friend Requests:<br />
+    <asp:Label ID="lb_invited" runat="server" ></asp:Label>
+
+    Requested Friends:<br />
+    <asp:Label ID="lb_invites" runat="server" ></asp:Label>
+
+    Friends:<br />
+    <asp:Label ID="lb_friends" runat="server" ></asp:Label>
+ 
     
     </div>
     </form>    
--- iis/iis/Friends.aspx.cs
+++ iis/iis/Friends.aspx.cs
@@ -5,13 +5,214 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Data.Odbc;
+
 namespace iis
 {
     public partial class Friends : System.Web.UI.Page
     {
+        OdbcConnection conn;
+        OdbcDataReader reader;
+        OdbcCommand cmd;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["user_id"] == null)
+            {
+                Response.Redirect("login.aspx");
+            }
+            Page.Header.Title = "SetBook - Login";
 
+            lb_friends.Text = get_friends();
+            lb_invited.Text = get_invited();
+            lb_invites.Text = get_invites();
+
+
+            //get list of friends form server
+        }
+
+        protected string get_invites()
+        {
+            string invite = "";
+            OdbcDataReader reader2;
+            OdbcCommand cmd2;
+
+            int count = 0;
+            string query = "SELECT firstfriend, secondfriend, status FROM friends WHERE (status = 'pending' AND (firstFriend='" + Session["user_id"] + "' OR secondFriend='" + Session["user_id"] + "'))";
+            string query2 = "";
+
+            conn = new OdbcConnection(Shared.ConnectionString());
+            cmd = new OdbcCommand(query);
+            cmd.Connection = conn;
+            conn.Open();
+
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string read = reader[1].ToString();
+                string id = Session["user_id"].ToString();
+
+                //user id is firstfriend it means they made request
+                if (read == id)
+                {
+                    query2 = "SELECT name, email, dateOfBirth FROM users WHERE id= '" + reader[0] + "' LIMIT 1";
+
+                    cmd2 = new OdbcCommand(query2);
+                    cmd2.Connection = conn;
+
+                    reader2 = cmd2.ExecuteReader();
+
+                    //if name cannot be retrieved
+                    if (reader2.HasRows == false)
+                    {
+                        invite += "Failed to get username for user" + reader[1] + "<br\\>";
+                    }
+
+                    //name found
+                    else
+                    {
+                        invite +=
+                            "<form action = '/friends.php' method = 'POST' />" + 
+                            "<fieldset >" + 
+                            "<legend> <a href = \"profile.aspx?id=" + reader[0] + 
+                            "\" >"+ reader2[0] +"</a></legend>" + reader2[1] + "<br \\>Birth date:" + reader2[2] +
+                            "<br \\>" + "<input type=\"hidden\"value='" + reader[0] +
+                            "' name='action_id'/>" + "<input type=\"submit\"value=\"Accept Request\" name='accept'/>" +
+                            "<input type=\"submit\" value=\"Decline Request\"name='decline' />" + 
+                            "</fieldset> <br \\>" + "</form>";
+                    }
+                }
+            }
+            return invite;
+        }
+
+        protected string get_invited()
+        {
+            string invite = "";
+            OdbcDataReader reader2;
+            OdbcCommand cmd2;
+
+            int count = 0;
+            string query = "SELECT firstfriend, secondfriend, status FROM friends WHERE (status = 'pending' AND (firstFriend='" + Session["user_id"] + "' OR secondFriend='" + Session["user_id"] + "'))";
+            string query2 = "";
+
+            conn = new OdbcConnection(Shared.ConnectionString());
+            cmd = new OdbcCommand(query);
+            cmd.Connection = conn;
+            conn.Open();
+
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string read = reader[0].ToString();
+                string id = Session["user_id"].ToString();
+
+                //user id is firstfriend it means they made request
+                if (read == id)
+                {
+                    query2 = "SELECT name, email, dateOfBirth FROM users WHERE id= '" + reader[1] + "' LIMIT 1";
+
+                    cmd2 = new OdbcCommand(query2);
+                    cmd2.Connection = conn;
+
+                    reader2 = cmd2.ExecuteReader();
+
+                    //if name cannot be retrieved
+                    if (reader2.HasRows == false)
+                    {
+                        invite += "Failed to get username for user" + reader[1] + "<br\\>";
+                    }
+
+                    //name found
+                    else
+                    {
+                        invite += "<fieldset ><legend> <a href = \"profile.aspx?id=" + reader[1] + "\" >" + reader2[0] + "</a></legend> </fieldset> <br \\>";
+                    }
+                }
+            }
+
+            
+            return invite;
+        }
+
+        protected string get_friends()
+        {
+            OdbcDataReader reader2;
+            OdbcCommand cmd2;
+
+            int count = 0;
+            string query = "SELECT firstfriend, secondfriend, status FROM friends WHERE (status = 'friends' AND (firstFriend='"+ Session["user_id"] + "' OR secondFriend='" + Session["user_id"] + "'))";
+            string query2 = "";
+            string friend = "";
+
+            conn = new OdbcConnection(Shared.ConnectionString());
+            cmd = new OdbcCommand(query);
+            cmd.Connection = conn;
+            conn.Open();
+
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                //int id = Convert.ToInt32( Session["user_id"].ToString());
+                string read = reader[0].ToString();
+                string id = Session["user_id"].ToString();
+
+                //user id is firstfriend
+                if (read == id)
+                {
+                    query2 = "SELECT name, email, dateOfBirth FROM users WHERE id= '" + reader[1] + "' LIMIT 1";
+
+                    cmd2 = new OdbcCommand(query2);
+                    cmd2.Connection = conn;
+
+                    reader2 = cmd2.ExecuteReader();
+
+                    //if name cannot be retrieved
+                    if (reader2.HasRows == false)
+                    {
+                        friend += "Failed to get username for user" + reader[1] + "<br\\>";
+                    }
+
+                    //name found
+                    else
+                    {
+                        friend += "<fieldset ><legend> <a href = \"profile.aspx?id=" + reader[1] + "\" >" + reader2[0] + "</a></legend>" + reader2[1] + "<br \\> Birth date:" + reader2[2] + "</fieldset> <br \\>";
+
+                    }
+
+
+                }
+
+                //user id is second friend
+                else
+                {
+                    query2 = "SELECT name, email, dateOfBirth FROM users WHERE id= '" + reader[0] + "' LIMIT 1";
+
+                    cmd2 = new OdbcCommand(query2);
+                    cmd2.Connection = conn;
+
+                    reader2 = cmd2.ExecuteReader();
+
+                    //if name cannot be retrieved
+                    if (reader2.HasRows == false)
+                    {
+                        friend += "Failed to get username for user" + reader[0] + "<br\\>";
+                    }
+
+                    //name found
+                    else
+                    {
+                        friend += "<fieldset ><legend> <a href = \"profile.aspx?id=" + reader[0] + "\" >" + reader2[0] + "</a></legend>" + reader2[1] + "<br \\> Birth date:" + reader2[2] + "</fieldset> <br \\>";
+
+                    }
+                }
+                count++;
+            }
+            if (count == 0)
+            {
+                friend = "No confirmed Friends <br/>";
+            }
+            return friend;
         }
     }
 }
