1bcd2e05df195be239cce1ac6ebb7f16c24b454a
Copeland <Copeland@Copeland-PC>
2011-04-12 21:29:01 -0400

    started to add friend acceptance

--- iis/iis/Friends.aspx.cs
+++ iis/iis/Friends.aspx.cs
@@ -22,10 +22,13 @@ namespace iis
                 Response.Redirect("login.aspx");
             }
             Page.Header.Title = "SetBook - Login";
+            accept_friend();
+            add_request();
+            
 
             lb_friends.Text = get_friends();
-            lb_invited.Text = get_invited();
-            lb_invites.Text = get_invites();
+            lb_invites.Text = get_invited();
+            lb_invited.Text = get_invites();
 
 
             //get list of friends form server
@@ -76,14 +79,83 @@ namespace iis
                             "<fieldset >" + 
                             "<legend> <a href = \"profile.aspx?id=" + reader[0] + 
                             "\" >"+ reader2[0] +"</a></legend>" + reader2[1] + "<br \\>Birth date:" + reader2[2] +
-                            "<br \\>" + "<input type=\"hidden\"value='" + reader[0] +
-                            "' name='action_id'/>" + "<input type=\"submit\"value=\"Accept Request\" name='accept'/>" +
+                            "<br \\>" + "<input type=\"hidden\"value='" + reader[0] + "' name='action_id'/>" + 
+                            "<input type=\"submit\"value=\"Accept Request\" name='accept'/>" +
                             "<input type=\"submit\" value=\"Decline Request\"name='decline' />" + 
                             "</fieldset> <br \\>" + "</form>";
                     }
                 }
             }
             return invite;
+        }
+
+        protected void add_request()
+        {
+
+            OdbcConnection connect;
+            OdbcDataReader read;
+            OdbcCommand command;
+            OdbcCommand command2;
+            string query;
+        
+
+            if ((Request["id"] != null) && (Request["id"] != Session["user_id"]))
+            {
+                query = "SELECT count(*) FROM friends WHERE (firstfriend='" + Request["id"]   + "' AND secondfriend='" + Session["user_id"] + "' ) OR (secondfriend='" + Session["user_id"] + "' AND firstfriend='" + Request["id"] + "')";
+                connect = new OdbcConnection(Shared.ConnectionString());
+                command = new OdbcCommand(query);
+                command.Connection = conn;
+                conn.Open();
+                read = command.ExecuteReader();
+                while (read.Read())
+                {
+                    if(read[0] == "0")
+                    {
+                        query = "INSERT INTO friends (firstFriend, secondFriend, status) VALUES (" + Session["user_id"] + "," + Request["id"] + ",'pending');";
+                        command2 = new OdbcCommand(query);
+                        command.Connection = connect;
+
+                    }
+                }
+                connect.Close();
+
+            }
+        }
+
+        protected void accept_friend()
+        {
+            OdbcConnection connect;
+            OdbcDataReader read;
+            OdbcCommand command;
+            string query;
+
+
+            //accepted request
+            if ((Request["accept_friend"] != null) && (Request["action_id"] != null))
+            {
+                query = "UPDATE friends SET status = 'friend' WHERE (firstfriend ='" + Request["action_id"] + "' AND secondfriend ='" + Request["user_id"] + "' ) OR (firstfriend ='" + Request["user_id"] + "' AND secondfriend ='" + Request["action_id"] + "' )";
+                connect = new OdbcConnection(Shared.ConnectionString());
+                command = new OdbcCommand(query);
+                command.Connection = connect;
+                connect.Open();
+                command.ExecuteReader();
+                connect.Close();
+            }
+
+            //declined request
+            else if ((Request["decline"] != null) && (Request["action_id"] != null))
+            {
+                query = "DELETE FROM friends WHERE (firstfriend ='" + Request["action_id"] + "' AND secondfriend ='" + Request["user_id"] + "' ) OR (firstfriend ='" + Request["user_id"] + "' AND secondfriend ='" + Request["action_id"] + "' )";
+                connect = new OdbcConnection(Shared.ConnectionString());
+                command = new OdbcCommand(query);
+                command.Connection = connect;
+                connect.Open();
+                command.ExecuteReader();
+                connect.Close();
+            }
+
+
+
         }
 
         protected string get_invited()
